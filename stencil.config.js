73c65405bd263b95b6894508cd5b2def8ecf2c57
Maxi Nivoli <m_nivoli@hotmail.com>
2018-02-26 13:31:28 -0300

    update to stencil 0.6.8

--- stencil.config.js
+++ stencil.config.js
@@ -1,6 +1,11 @@
+const sass = require('@stencil/sass');
+
 exports.config = {
   namespace: 'wopvincent',
-  generateDistribution: true
+  generateDistribution: true,
+  plugins: [
+    sass()
+  ]
 };
 
 exports.devServer = {
